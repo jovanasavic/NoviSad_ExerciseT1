e878a2f1c10efdd2afc739d3be96b89858d605c7
somename <someemail>
2017-05-11 20:15:05 +0200

    besouro automatic message

--- tests/TestBowling.java
+++ tests/TestBowling.java
@@ -4,8 +4,12 @@ import org.junit.Test;
 
 public class TestBowling {
 
-	@Test
-	public void test() {
+	@Test(expected = BowlingException.class)
+	public void test() throws BowlingException {
+		
+		for (int i=0; i<11;i++)
+			new Frame(2,2);
+		
 		fail("Not yet implemented");
 	}
 
